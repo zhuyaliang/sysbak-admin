9549321e055d9281e203ab4a1282d4e2ce47d869
zhuyaliang <15132211195@163.com>
2019-09-03 15:43:47 +0800

    Modify README and test program file format

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# libpartclone
+#sysbak-admin
 
 硬盘/分区备份还原库
+
+##ext/2/3/4 文件系统
+
+###备份分区到分区
+```sysbak_extfs_ptf_async```
+
+###备份分区到文件
+```sysbak_extfs_ptf_async```
+
+###恢复备份
+```sysbak_extfs_restore_async```
+
+##test 使用说明
+```test目录下是测试历程，make 后可直接使用test.ini是测试配置文件，测试时需要修改选项,(使用root权限测试)```
+
+##编译
+```meson build -Dprefix=/usr```
+
+```ninja -C build```
+
+```sudo ninja -C build install```
--- test/main.c
+++ test/main.c
@@ -1,3 +1,19 @@
+/*  sysbak-admin 
+*   Copyright (C) 2019  zhuyaliang https://github.com/zhuyaliang/
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -19,17 +35,17 @@ static void call_finished (GObject      *source_object,
                            gpointer      f_data)
 {
     device_info *dev_info;
-	GError *error = NULL;
+    GError *error = NULL;
 
-	dev_info = sysbak_extfs_finish (res,&error);
-	if (dev_info == NULL)
-	{
-		g_print ("%s Faild %s\r\n",(const char *)f_data,error->message);
-	}
-	else
-	{
-		g_print ("file system type:  Successful %s\r\n",dev_info->fs);
-	}
+    dev_info = sysbak_extfs_finish (res,&error);
+    if (dev_info == NULL)
+    {
+        g_print ("%s Faild %s\r\n",(const char *)f_data,error->message);
+    }
+    else
+    {
+        g_print ("file system type:  Successful %s\r\n",dev_info->fs);
+    }
 }    
 
 void call_progress (progress_data *data,gpointer p_data)
@@ -40,77 +56,77 @@ void call_progress (progress_data *data,gpointer p_data)
 
 static void start_test_ptf (char *source,char *targer)
 {
-	GCancellable *cancellable;
+    GCancellable *cancellable;
     const char *data = "ptf";
-    
+
     cancellable = g_cancellable_new ();
     g_print ("start Partition to File Backup \r\n");
     if (!sysbak_extfs_ptf_async ((const char *)source, 
                                  (const char *)targer,
-                                 0,
-                                 cancellable,
-                                 call_finished,
+                                  0,
+                                  cancellable,
+                                  call_finished,
                                  (gpointer)data,
-                                 call_progress,
+                                  call_progress,
                                  (gpointer)data))
-	{
-	    g_print ("Partition-to-File backup failed \r\n");
-	} 
+    {
+        g_print ("Partition-to-File backup failed \r\n");
+    } 
     free (source);
     free (targer);
-	g_object_unref (cancellable);
+    g_object_unref (cancellable);
 }    
 
 static void start_test_ptp (char *source,char *targer)
 {
-	GCancellable *cancellable;
+    GCancellable *cancellable;
     const char *data = "ptp";
 
     cancellable = g_cancellable_new ();
     g_print ("start Partition to Partition Backup \r\n");
-    
+
     if (!sysbak_extfs_ptp_async ((const char *)source, 
                                  (const char *)targer,
-                                 0,
-                                 cancellable,
-                                 call_finished,
+                                  0,
+                                  cancellable,
+                                  call_finished,
                                  (gpointer)data,
-                                 call_progress,
+                                  call_progress,
                                  (gpointer)data))
-	{
-	    g_print ("Partition-to-partition backup failed \r\n");
-	} 
+    {
+        g_print ("Partition-to-partition backup failed \r\n");
+    } 
     free (source);
     free (targer);
-	g_object_unref (cancellable);
+    g_object_unref (cancellable);
 }    
 
 static void start_test_restore (char *source,char *targer)
 {
-	GCancellable *cancellable;
+    GCancellable *cancellable;
     const char *data = "restore";
-    
+
     cancellable = g_cancellable_new ();
     g_print ("start resatore backup from files \r\n");
     if (!sysbak_extfs_restore_async ((const char *)source, 
                                      (const char *)targer,
-                                     0,
-                                     cancellable,
-                                     call_finished,
+                                      0,
+                                      cancellable,
+                                      call_finished,
                                      (gpointer)data,
-                                     call_progress,
+                                      call_progress,
                                      (gpointer)data))
-	{
-	    g_print ("resatore backup from files failed \r\n");
-	} 
+    {
+        g_print ("resatore backup from files failed \r\n");
+    } 
     free (source);
     free (targer);
-	g_object_unref (cancellable);
+    g_object_unref (cancellable);
 }    
 int main(int argc, char **argv)
 {
     GKeyFile     *kconfig = NULL;
-	GMainLoop    *loop;
+    GMainLoop    *loop;
     GError       *error = NULL;
     g_auto(GStrv) test_groups = NULL;
     gsize         length = 0;
@@ -125,7 +141,7 @@ int main(int argc, char **argv)
         {"enable_restore",start_test_restore}
     };
 
-	loop = g_main_loop_new (NULL, FALSE);
+    loop = g_main_loop_new (NULL, FALSE);
     kconfig = g_key_file_new();
     if(kconfig == NULL)
     {
@@ -156,7 +172,7 @@ int main(int argc, char **argv)
         source = g_key_file_get_string(kconfig,
                                        test_groups[i],
                                       "source",
-                                       &error);
+                                      &error);
         if (source == NULL)
         {
             g_warning ("test group %s key source no setting\r\n",test_groups[i]);
@@ -165,27 +181,28 @@ int main(int argc, char **argv)
         targer = g_key_file_get_string(kconfig,
                                        test_groups[i],
                                       "targer",
-                                       &error);
+                                      &error);
         if (targer == NULL)
         {
             g_warning ("test group %s key targer no setting\r\n",test_groups[i]);
+            free (source);
             continue;
         }   
         g_print ("source = %s targer = %s\r\n",source,targer);
         array_test_data[i].start_test(source,targer);
         mode++;
     }    
-    
+
     if (mode == 0 )
     {
         g_print ("You haven't done anything. Please configure the test.ini file to open the test options.\r\n");
     }    
-	g_main_loop_run (loop);
+    g_main_loop_run (loop);
 ERROR:
     if (kconfig != NULL)
     {
         g_key_file_free (kconfig);
     }    
     g_main_loop_unref (loop);
-	g_main_loop_quit (loop);
+    g_main_loop_quit (loop);
 }    
